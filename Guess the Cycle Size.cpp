964cc33aa6e82a628232385583bca512158fe72f
Sabbir <hsabbir1001@gmail.com>
2022-09-14 03:16:24 +0600

    14-sep-22 practice

--- Guess the Cycle Size.cpp
+++ Guess the Cycle Size.cpp
@@ -15,22 +15,32 @@ int main() {
 
 
 
-  ll num=3,ans=2,num2=1;
+  ll num=1,ans=2,num2=4;
 
-   for(int i=0; i<50; i++)
+   for(int i=0; i<25; i++)
    {
    	 cout<<"? "<<num2<<" "<<num<<endl;
    	 fflush(stdout);
    	 ll tmp;
    	 cin>>tmp;
-   	 ans=max(ans,tmp);
-   	 if(tmp==-1)
-   	 {
 
+     cout<<"? "<<num<<" "<<num2<<endl;
+     fflush(stdout);
+     ll tmp2;
+     cin>>tmp2;
+
+   	 if(tmp==-1 || tmp2==-1)
+   	 {
+      ans=num2-1;
+      break;
    	 }
-   	 swap(num,num2);
+     else if(tmp==tmp2)num2++;
+     else {
+     ans=tmp2+tmp;
+     break;
+   	 }
    }
-   cout<<"! "<<ans+1<<endl;
+   cout<<"! "<<ans<<endl;
    fflush(stdout);
 
 }
